f918e9d747b48d189a4c2b409e8f3dcf25b0088c
mauricio ocampo marulanda <maoca2085@gmail.com>
2022-10-11 19:22:53 -0500

    Clase de function scope

---
+++ src/scope/function.js
@@ -0,0 +1,11 @@
+function greeting() {
+    let userName = 'Ana'; // Declaración de variable de función o local
+    console.log(userName);
+
+    if(userName === 'Ana') {
+        console.log(`Hello ${userName}!`)
+    }
+}
+
+greeting();
+console.log(userName);
