f17f4b8e9591092a694cdc47cb24e6f10d9b8651
mauricio ocampo marulanda <maoca2085@gmail.com>
2022-10-11 20:41:12 -0500

    Clase de reasignacion y redeclaracion

---
+++ src/scope/re.js
@@ -0,0 +1,38 @@
+var firstName; // Undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+// variable tipo var 
+
+var lastName = 'David'; // declarar / asignar
+lastName = 'Ana'; // reasignar
+console.log(lastName);
+
+var secondName = 'David'; // declarar / asignar
+var secondName = 'Ana'; // redeclarar / reasignar
+console.log(secondName);
+
+// let
+
+let fruit = 'Apple'; // declarar / asignar
+fruit = 'Kiwi'; // reasignar
+console.log(fruit);
+
+let fruit = 'Banana'; // declarar / asignar
+console.log(fruit);
+
+// const
+
+const animal = 'Perro'; // declarar / asignar
+animal = 'Caballo'; // reasignar
+console.log(animal);
+
+const animal = 'Snake'; // redeclarar / reasignar => No se puede ECMA6
+console.log(fruit);
+
+const vehicles = [];
+vehicles.push("carrito");
+console.log(vehicles);
+
+vehicles.pop("carrito");
+console.log(vehicles);
